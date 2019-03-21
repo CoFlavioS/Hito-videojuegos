30a0ee079507b699b5ea4b4feee5cbea8caeeeb4
CoFlavioS <jaguaru40@yahoo.es>
2019-03-22 00:52:48 +0100

    Point system by time added and minor repairs.

---
+++ Basic Platformer/Assets/Scripts/Points.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour
+{
+    public Text waveCountdownText;
+    public float puntuacion = 0f;
+
+    // Update is called once per frame
+    void Start()
+    {
+        InvokeRepeating("addPoints", 2f, 1f);
+    }
+
+    void addPoints()
+    {
+        puntuacion += 100f;
+        waveCountdownText.text = Mathf.Round(puntuacion).ToString();
+    }
+
+}
